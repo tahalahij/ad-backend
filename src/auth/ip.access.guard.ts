aebbe939eb42fc7420b8a8af59ea3ad27157c8d9
taha <tahalahij@gmail.com>
2023-05-22 07:17:59 +0330

    fix: remove role check for ip.access.guard.ts

--- src/auth/ip.access.guard.ts
+++ src/auth/ip.access.guard.ts
@@ -9,12 +9,6 @@ export class IpAccessCheckGuard implements CanActivate {
   constructor(@Inject(DeviceService) private deviceService: DeviceService) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const user = request.user;
-    const role = user.role;
-
-    if (role === RolesType.ADMIN) {
-      return true;
-    }
 
     let requestIp = request.ip;
     if (requestIp.slice(0, 7) == '::ffff:') {
