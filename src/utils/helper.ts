33f0e00cc8cbd7a91f82a0b64325f2128ea5b096
taha <tahalahij@gmail.com>
2023-11-21 00:54:05 +0330

    fix: regex for like

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,3 @@
-
-
 export function handleIPV6(ip: string) {
   if (ip.slice(0, 7) == '::ffff:') {
     return ip.slice(7, ip.length);
@@ -11,7 +9,7 @@ export function handleIPV6(ip: string) {
 export function persianStringJoin(arr: string[]): string {
   return arr.join();
 }
-export function likeRegx(value:string) {
- const  regex = new RegExp(["/", value, "/"].join(""), "i");
-  return regex
+export function likeRegx(value: string) {
+  const regex = new RegExp(`/${value}/`, 'i');
+  return regex;
 }
